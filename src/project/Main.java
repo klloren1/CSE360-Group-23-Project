46e067e9519f5025c32a6baa6bb1e84411adf1b5
Jmbucha1 <jmbucha1@asu.edu>
2016-03-31 14:16:41 -0700

    Update Main.java

--- src/project/Main.java
+++ src/project/Main.java
@@ -6,6 +6,9 @@ package project;
  * @author Kyle Lorenzano PIN: 517
  *
  * Edit showing ability to use Group GitHub @author Kevin Patterson PIN: 625
+ * 
+ * Edit showing ability to use Group GitHub @author Jacob Buchanan PIN: 203
+ * 
  */
 
 public class Main {
