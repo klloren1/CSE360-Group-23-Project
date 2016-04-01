301d1e4722b06bfbaa951748fa8a3e09118074ce
jpgardnerhajek <jay6gh@gmail.com>
2016-04-01 10:44:40 -0700

    Update Main.java

--- CSE360-Group-23-Project/src/project/Main.java
+++ CSE360-Group-23-Project/src/project/Main.java
@@ -10,6 +10,8 @@ package project;
  * Edit showing ability to use Group GitHub @author Jacob Buchanan PIN: 203
  * 
  * Edit showing ability to use Group GitHub @author Katelyn Alexander PIN: 103
+ * 
+ * Edit showing ability to use Group GitHub @author James Gardner-Hajek PIN: 318
  */
 
 public class Main {
