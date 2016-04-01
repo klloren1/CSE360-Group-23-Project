a03d2d59c8fc1e9306161c3f2d08e56523848afe
katelyna <katelyna@katelyna-HP>
2016-03-31 21:07:53 -0700

    First commit- Katelyn Alexander

---
+++ CSE360-Group-23-Project/src/project/Main.java
@@ -0,0 +1,18 @@
+package project;
+
+/**
+ * Test class for first commit.
+ * 
+ * @author Kyle Lorenzano PIN: 517
+ *
+ * Edit showing ability to use Group GitHub @author Kevin Patterson PIN: 625
+ * 
+ * Edit showing ability to use Group GitHub @author Jacob Buchanan PIN: 203
+ * 
+ * Edit showing ability to use Group GitHub @author Katelyn Alexander PIN: 103
+ */
+
+public class Main {
+
+
+}
